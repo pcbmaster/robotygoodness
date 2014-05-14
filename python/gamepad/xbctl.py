a840ffe9d4e44ec07865cffbcdda9e41c30b1a71
Brett Monty <fitnessnonsense@gmail.com>
2014-05-14 16:19:30 -0600

    fixed port and stuffs in xbctl

--- python/gamepad/xbctl.py
+++ python/gamepad/xbctl.py
@@ -1,17 +1,21 @@
 import gamepad, socket, time
 
+ip = "192.168.80.12"
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+old = []
+gdata = []
 while 1:
-    while 1:
+    while old == gdata:
         gdata = gamepad.get()
-        if gdata[0] > 5:
-            s.sendto("R10255",("127.0.0.1",7877))
-        elif gdata[0] < -5:
-            s.sendto("R01255",("127.0.0.1",7877))
-        elif gdata[1] < -5:
-            s.sendto("R11255",("127.0.0.1",7877))
-        elif gdata[1] > 5:
-            s.sendto("R00255",("127.0.0.1",7877))
-        else:
-            s.sendto("R22255",("127.0.0.1",7877))
+    old = gdata
+    if gdata[0] > 5:
+        s.sendto("R10255",(ip,7777))
+    elif gdata[0] < -5:
+        s.sendto("R01255",(ip,7777))
+    elif gdata[1] < -5:
+        s.sendto("R11255",(ip,7777))
+    elif gdata[1] > 5:
+        s.sendto("R00255",(ip,7777))
+    else:
+        s.sendto("R22255",(ip,7777))
